b2b232611c9870c8dad8c609e6630b67ddfc8bf8
Ciaran Mahoney <ciaran@zhoom.com.au>
2014-10-07 15:03:05 -0700

    fixing 404 and body background file path

--- 404.php
+++ 404.php
@@ -7,8 +7,8 @@
 					<div class="the-content">
 						<p class="content">Maybe the url is incorrect or the page has moved... Sorry about that... As a consolation, here's Charlie. </p>
 
-						<p class="content">You can try going back up and clicking a link on the menu or look below Charlie for a full list of pages on this website.</p>
-						
+						<p class="content">You can try going back up and clicking a link on the menu or look below Charlie for a full list of pages.</p> 
+
 						<img src="<?php bloginfo( 'template_url' ); ?>/img/404-image.jpg"/>
 						<p class="wp-caption-text">Photo Credit: <a href="https://www.flickr.com/photos/ciaranmahoney/6087844508/in/set-72157627320395073">Ciaran Mahoney</a> via Flickr <a href="https://creativecommons.org/licenses/by-nc-sa/2.0/">cc</a></p>
 					
