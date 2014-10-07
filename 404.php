66408972719f2f9e5cfcd748943a5764a0d6fbcd
Ciaran Mahoney <ciaran@zhoom.com.au>
2014-10-07 14:23:07 -0700

    CLosing div tag on 404

--- 404.php
+++ 404.php
@@ -4,9 +4,10 @@
 				
 				<article class="post error">
 					<h1 class="404 title">Sorry, this page doesn't exist. 404 error!</h1>
-					<div class="the-content"
-						<p class="content">Maybe the url is incorrect or the page has moved... Sorry about that... As a consolation, here's Charlie.</p>
-						<p class="content">You can try going back up and clicking a link on the menu or see below for a full list of pages on this website.</p>
+					<div class="the-content">
+						<p class="content">Maybe the url is incorrect or the page has moved... Sorry about that... As a consolation, here's Charlie. </p>
+
+						<p class="content">You can try going back up and clicking a link on the menu or look below Charlie for a full list of pages on this website.</p>
 						
 						<img src="<?php bloginfo( 'template_url' ); ?>/img/404-image.jpg"/>
 						<p class="wp-caption-text">Photo Credit: <a href="https://www.flickr.com/photos/ciaranmahoney/6087844508/in/set-72157627320395073">Ciaran Mahoney</a> via Flickr <a href="https://creativecommons.org/licenses/by-nc-sa/2.0/">cc</a></p>
